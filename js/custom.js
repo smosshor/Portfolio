82e42996898565b78d9669cdc0227c342191b34f
smosshor <Sarah@Sarahs-MacBook-Air.local>
2017-01-08 13:36:03 -0500

    k

--- js/custom.js
+++ js/custom.js
@@ -6,17 +6,18 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
-$(document).ready(function() {
-$('.thumbnail').click(function(){
-      $('.modal-body').empty();
-  	var title = $(this).parent('a').attr("title");
-  	$('.modal-title').html(title);
-  	$($(this).parents('div').html()).appendTo('.modal-body');
-  	$('#myModal').modal({show:true});
-});
-});
-
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
+});
+
+$(this).ekkoLightbox({
+    alwaysShowClose: true,
+    onShown: function() {
+        console.log('Checking our the events huh?');
+    },
+    onNavigate: function(direction, itemIndex)
+        console.log('Navigating '+direction+'. Current item: '+itemIndex);
+    }
+    ...
 });
