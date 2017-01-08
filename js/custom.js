8b1b0093e9a3b562d7c548f9d38f1c9b61ce08a6
smosshor <Sarah@Sarahs-MacBook-Air.local>
2017-01-08 12:52:10 -0500

    r

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,12 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+$(document).ready(function() {
+$('.thumbnail').click(function(){
+      $('.modal-body').empty();
+  	var title = $(this).parent('a').attr("title");
+  	$('.modal-title').html(title);
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
+});
