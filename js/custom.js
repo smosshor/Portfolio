0b7d016ca1b0827ec9ace1a4b604b3aa21a065bd
smosshor <smosshorwitz@gmail.com>
2017-08-08 13:10:33 -0400

    s

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,8 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 200) {
-    $('navbar-fixed-top').addClass('shrink');
+  if ($(document).scrollTop() > 100) {
+    $('.navbar-fixed-top').addClass('shrink');
   } else {
-    $('nav .nav-wrapper').removeClass('shrink');
+    $('.navbar-fixed-top').removeClass('shrink');
   }
 });
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 200) {
-    $('navbar-fixed-top .brand-logo').addClass('shrink');
-  } else {
-    $('nav .brand-logo').removeClass('shrink');
-  }
-});
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 200) {
-    $('nav ul a').addClass('shrink');
-  } else {
-    $('nav ul a').removeClass('shrink');
-  }
-});
-
-
-
-$( document ).ready(function() {
-    
-$("#buttonDropDown").click(function(){
-    $("#section1").toggle();
-});
-});
-
 
