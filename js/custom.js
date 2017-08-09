8a4dbdc0564b203b44e2928aa2c626352ecc15ab
Sarah Moss-Horwitz <smosshorwitz@gmail.com>
2017-08-09 12:39:52 -0400

    a

--- js/custom.js
+++ js/custom.js
@@ -9,21 +9,5 @@ $(window).scroll(function() {
   }
 });
 
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-        return this;
-    }
-});
 
-AOS.init({
-  duration: 1200,
-})
-$('.col-md-4').AOS('fade-up');
-$('.col-md-8').AOS('fade-up');
-$('.col-md-12').AOS('fade-up');
-    
     });
