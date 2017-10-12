9ba9c44c1cbe77f3e4b4cecbd13c68dd71cf9268
Sarah MH <shorwitz@gofundme.com>
2017-10-12 16:38:55 -0400

    s

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,3 @@
-jQuery( document ).ready(function( $ ) {
-
-//paste this code under the head tag or in a separate js file.
-// Wait for window load
-$(window).load(function() {
-    // Animate loader off screen
-    $(".load-page").fadeOut("slow");;
-});
-/*    
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('.fixed-top').addClass('shrink');
-  } else {
-    $('.fixed-top').removeClass('shrink');
-  }
-});
-    
-$(window).scroll(function() {
-  if ($(document).scrollTop() >50) {
-    $('.navbar-nav.mr-auto').addClass('shrink');
-  } else {
-    $('.navbar-nav.mr-auto').removeClass('shrink');
-  }
-});   
-    */
-
+$(document).ready(function () {
+    $("img").addClass("wow animated slideInUp");
 });
